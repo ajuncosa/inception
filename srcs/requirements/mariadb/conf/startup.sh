af9f1281002624ec0099266ff4c7470352eb5942
Ana Juncosa montes <ajm.zetazix@gmail.com>
2022-11-15 12:52:18 +0100

    add: env variables

--- srcs/requirements/mariadb/conf/startup.sh
+++ srcs/requirements/mariadb/conf/startup.sh
@@ -3,5 +3,5 @@
 ./usr/share/mariadb/mysql.server start
 mariadb -e "SET PASSWORD FOR root@localhost=PASSWORD('$MYSQL_ROOT_PASSWORD');"
 mariadb -e "SET PASSWORD FOR mysql@localhost=PASSWORD('$MYSQL_PASSWORD');"
-mariadb -e "CREATE USER 'wp'@'%' IDENTIFIED BY '$WP_PASSWORD';"
-mariadb -e "GRANT ALL PRIVILEGES ON ajuncosadb.* TO 'wp'@'%' WITH GRANT OPTION;"
+mariadb -e "CREATE USER '$WP_DB_USER'@'%' IDENTIFIED BY '$WP_PASSWORD';"
+mariadb -e "GRANT ALL PRIVILEGES ON $WP_DB_NAME.* TO '$WP_DB_USER'@'%' WITH GRANT OPTION;"
--- srcs/requirements/wordpress/conf/startup.sh
+++ srcs/requirements/wordpress/conf/startup.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
-wp config create --path=wordpress/ --dbname=ajuncosadb --dbuser=wp --dbpass=wp-pass --dbhost=mariadb
+wp config create --path=wordpress/ --dbname=$WP_DB_NAME --dbuser=$WP_DB_USER --dbpass=$WP_PASSWORD --dbhost=$MARIADB_NET
 wp db create --path=wordpress/
-wp core install --path=wordpress/ --url=ajuncosa.42.fr --title="La web de ajuncosa" --admin_user=ajuncosa --admin_password=hola --admin_email=ajuncosa@hola.adios --skip-email
+wp core install --path=wordpress/ --url=ajuncosa.42.fr --title="La web de ajuncosa" --admin_user=$WP_ADMIN_USER --admin_password=$WP_ADMIN_PWD --admin_email=ajuncosa@hola.adios --skip-email
 
 exec ./usr/sbin/php-fpm8 --nodaemonize
