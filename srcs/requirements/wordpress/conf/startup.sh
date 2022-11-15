88a6f76d987879ea4ccd7e5486c0e6e8d22d5a79
Ana Juncosa montes <ajm.zetazix@gmail.com>
2022-11-15 16:32:41 +0100

    add: other user create wordpress

--- srcs/requirements/wordpress/conf/startup.sh
+++ srcs/requirements/wordpress/conf/startup.sh
@@ -2,6 +2,7 @@
 
 wp config create --path=wordpress/ --dbname=$WP_DB_NAME --dbuser=$WP_DB_USER --dbpass=$WP_PASSWORD --dbhost=$MARIADB_NET
 wp db create --path=wordpress/
-wp core install --path=wordpress/ --url=ajuncosa.42.fr --title="La web de ajuncosa" --admin_user=$WP_ADMIN_USER --admin_password=$WP_ADMIN_PWD --admin_email=ajuncosa@hola.adios --skip-email
+wp core install --path=wordpress/ --url=$DOMAIN_NAME --title="La web de ajuncosa" --admin_user=$WP_ADMIN_USER --admin_password=$WP_ADMIN_PWD --admin_email=$WP_ADMIN_EMAIL --skip-email
+wp user create $WP_OTHER_USER $WP_OTHER_EMAIL --role=$WP_OTHER_ROLE --user_pass=$WP_OTHER_PWD --path=/wordpress
 
 exec ./usr/sbin/php-fpm8 --nodaemonize
